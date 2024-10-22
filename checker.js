6323fa88c50b4c41bc1b00d577c4454b1edcbee8
Andrey Suvorov <suvorov.av93@yandex.ru>
2024-10-22 09:08:15 +0300

    fix

--- checker.js
+++ checker.js
@@ -3,15 +3,20 @@ import fetch from 'node-fetch';
 const visitedLinks = new Set();  // Множество для хранения посещенных ссылок
 const errors = new Map()
 let checkedLinks = 0
-const initLink = 'https://developer.auroraos.ru/'
+
 // Проверка доступности ссылки
-async function checkLink(url, referrer) {
-    if (visitedLinks.has(url)) {
-        return;
+async function checkLink(url, referrer, level = 0) {
+    if(visitedLinks.has(url)) {
+        return
     }
-    url = url.replaceAll(/&amp;/g, '&');
+
     visitedLinks.add(url);
     checkedLinks += 1
+
+    // если вдруг вышли за пределы сайта, то выведем в лог
+    if(!referrer.startsWith(rootHost)) {
+        console.log(`${url} <- ${referrer} : total ${visitedLinks.size}`);
+    }
 
     try {
         const response = await fetch(url, {
@@ -19,81 +24,60 @@ async function checkLink(url, referrer) {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
             },
-            redirect: 'follow'
+            redirect: 'follow',
         });
 
         if (!response.ok) {
             errors.set(url, { "referrer": referrer, "status": response.status });
-            // console.log(url, errors.get(url))
+            console.log(url, errors.get(url))
             return;
         }
 
-        if (!url.startsWith(initLink)){
-          return;  
+        // Парсим только HTML и внутренние ссылки
+        if (!url.startsWith(rootHost) || !response.headers.get('content-type').match("text/html")) {
+            return;
         }
 
-        if (checkedLinks % 1000 == 0) {
+        if (checkedLinks % 100 === 0) {
             console.log(`Проверено ${checkedLinks} ссылок`)
         }
-
 
         const html = await response.text();
         const links = extractLinks(html, url);
         for (const link of links) {
-            await checkLink(link, url)
+             await checkLink(link, url, level + 1);
         }
 
     } catch (error) {
         errors.set(url, { "referrer": referrer, "status": error.message });
-        // console.log(url, errors.get(url))
+        console.log(url, errors.get(url))
     }
-
 }
 
 
 // Извлечение href и src из HTML-страницы
 function extractLinks(html, baseUrl) {
-    // Регулярное выражение для поиска ссылок и атрибутов src
-    const linkRegex = /href\s*=\s*["']([^"']+)["']/g;
-    const srcRegex = /src\s*=\s*["']([^"']+)["']/g;
-
+    const linkAndSrcRegex = /(?:href|src)\s*=\s*["']([^"']+)["']/g;
+    const result = new Set();
     let match;
 
-    const result = []
+    while ((match = linkAndSrcRegex.exec(html)) !== null) {
+        const url = match[1].replaceAll(/&amp;/g, '&');
 
-    // Поиск href
-    while ((match = linkRegex.exec(html)) !== null) {
-        const href = match[1];
-        if (href.startsWith('mailto:')) {
-            continue
+        if (!url.startsWith('mailto:')) {
+            // приводим ссылку к нормальному состоянию и убираем якорь
+            result.add((url.startsWith("https") ? url : new URL(url, baseUrl).href).split("#")[0]);
         }
-        result.push(fixLink(href, baseUrl));
     }
 
-    // Поиск src
-    while ((match = srcRegex.exec(html)) !== null) {
-        const src = match[1];
-        result.push(fixLink(src, baseUrl));
-    }
-    return result
+    return Array.from(result);
 }
-
-// Обработка ссылок
-function fixLink(href, baseUrl) {
-    if (href.startsWith(baseUrl)) {
-        return href
-    } else if (!href.startsWith('https:')) {
-        const absoluteUrl = new URL(href, baseUrl).href;
-        return absoluteUrl
-    }
-    return href
-}
-
 
 // Вывод ошибок
 function outputErrors() {
+    console.log(`Обработано ссылок: ${checkedLinks}`)
     if (errors.size > 0) {
-        console.log("Найдены ошибки:");
+        console.log(`Найдены ошибки: ${errors.size}`);
         errors.forEach((value, key) => {
             console.log(`URL: ${key}, Referrer: ${value.referrer}, Status: ${value.status}`);
         });
@@ -104,5 +88,6 @@ function outputErrors() {
 
 // Получение ссылки из аргументов командной строки и запуск скрипта
 const startUrl = process.argv[2];
+const rootHost = `${ new URL(startUrl).protocol}//${ new URL(startUrl).hostname}`;
 console.log("Checking...")
 checkLink(startUrl, 'Initial Link').then(outputErrors);
