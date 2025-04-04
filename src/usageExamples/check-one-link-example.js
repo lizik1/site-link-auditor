9d0174bd4ad80fa100894755b5b27b6f6bbad8e0
Elizaveta Vaysberg <e.vaysberg@omp.ru>
2025-04-04 10:44:56 +0300

    remove examples.

--- src/usageExamples/check-one-link-example.js
+++
@@ -1,8 +0,0 @@
-import { LinkChecker } from 'site-link-auditor';
-
-
-const startUrl = "https://YOUR-LINK.ru/";
-const linkChecker = new LinkChecker(startUrl, 1);
-const result = await linkChecker.run();
-console.log(result)
-// process.exit(0)
--- src/usageExamples/check-with-limited-recursion-depth.js
+++
@@ -1,9 +0,0 @@
-import { LinkChecker } from 'site-link-auditor';
-
-
-const startUrl = "https://YOUR-LINK.ru/";
-// Проверяем все ссылки на глубине 2
-const linkChecker = new LinkChecker(startUrl, 2);
-const result = await linkChecker.run();
-console.log(result)
-// process.exit(0)
